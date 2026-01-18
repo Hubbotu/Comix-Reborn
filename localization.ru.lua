3c185709a8cbcec2cbe370943719d6e99308aef8
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2026-01-18 13:44:42 +0300

    Add files via upload

---
+++ localization.ru.lua
@@ -0,0 +1,44 @@
+-- Russian Localization ZamestoTV --
+
+if ( GetLocale() == "ruRU" ) then
+	-- Abilities --
+COMIX_BS              = "Боевой крик"
+COMIX_DS              = "Деморализующий крик"
+COMIX_DR              = "Деморализующий рёв"
+COMIX_ICEBLOCK        = "Ледяная глыба"
+COMIX_CANNIBALIZE     = "Каннибализм"
+COMIX_SPRINT          = "Спринт"
+COMIX_DASH            = "Рывок"
+COMIX_TIGER_DASH      = "Рывок тигра"
+COMIX_ASPECT          = "Аспект гепарда"
+COMIX_STAMPEDING_ROAR = "Топот"
+	
+	-- Targets --
+COMIX_BIGGLESWORTH    = "Мистер Бигглсуорт"
+COMIX_REPAIRBOT       = "Полевой ремонтный робот 74A"
+COMIX_REPAIRBOT2      = "Полевой ремонтный робот 110G"
+COMIX_MUFFIN          = "Булочник Мозер"
+	
+	-- Objections --
+COMIX_OBJECT          = " ВОЗРАЖАЮ!"
+COMIX_OBJECTS         = " ВОЗРАЖАЮТ!"
+COMIX_OBJECTTO        = " возражаю " 
+COMIX_OBJECTSTO       = " возражают "
+	
+	-- Hug Meter --
+COMIX_YOUUP           = "Вы "               
+COMIX_HUG             = " обнимаете "
+COMIX_HUGS            = " обнимает " 
+COMIX_YOULOW          = " вас"            
+COMIX_NEED            = " нужно"            
+COMIX_HUGGAP          = 5                  
+COMIX_HUGSGAP         = 6
+	
+	-- Bad jokes --
+COMIX_BADJOKE         = " плохая шутка."
+COMIX_JOKEEMOTE1      = "чувствует плохую шутку."
+COMIX_JOKEEMOTE2A     = "закатывает глаза на "
+COMIX_JOKEEMOTE2B     = " из-за его плохой шутки."
+
+COMIX_DRAMAEMOTE      = "улавливает нотку драмы."
+end
